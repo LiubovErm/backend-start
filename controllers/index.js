362f2a6f224aeeb6ffb9262678e99ec5c3617480
SPoluliakh <S_Poluliakh@ukr.net>
2023-03-27 21:19:47 +0300

    added service model and rout

--- controllers/index.js
+++ controllers/index.js
@@ -2,12 +2,12 @@ const users = require("./users");
 // const news = require("./news");
 // const notices = require("./notices");
 // const pets = require("./pets");
-// const services = require("./services");
+const services = require("./services");
 
 module.exports = {
-    users,
-    // news,
-    // notices,
-    // pets,
-    // services
-}
+  users,
+  // news,
+  // notices,
+  // pets,
+  services,
+};
