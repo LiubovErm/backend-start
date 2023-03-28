d6778dffa3a24e83b5f0c2d7f315c980f0110dad
SPoluliakh <S_Poluliakh@ukr.net>
2023-03-28 15:58:53 +0300

    updated

--- controllers/services/getAllServices.js
+++ controllers/services/getAllServices.js
@@ -1,5 +1,11 @@
-const getAllServices = async (req, res) => {
+const { Service } = require("../../models/service");
 
+const getAllServices = async (_, res) => {
+  const services = await Service.find({}, "-createdAt -updatedAt");
+  res.json({
+    message: "Successfully",
+    services,
+  });
 };
 
 module.exports = getAllServices;
